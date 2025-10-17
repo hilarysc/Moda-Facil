87a5acfb22385899daaca5c07f0079c3b86353d5
hilarysc <hilarypere3@gmail.com>
2025-10-17 07:43:33 -0400

    algo

--- db/conectiondb.php
+++ db/conectiondb.php
@@ -1,4 +1,11 @@
 <?php
 
-    
+$conexion = new mysqli("localhost", "root", "", "tienda_juan");
+
+if($conexion->connect_errno) {
+    die("Conexion Fallida" . $conexion->connect_errno);
+} else {
+    echo "Conectado";
+}
+
 ?>
